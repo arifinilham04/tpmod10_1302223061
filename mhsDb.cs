438537d77a45c18159ae17d794268869e48e05a9
arifin <86230082+arifinilham04@users.noreply.github.com>
2024-04-28 21:03:32 +0700

    Add files via upload

---
+++ mhsDb.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using tpmod10_1302223061;
+
+internal class mhsDb : DbContext
+{
+    public mhsDb(DbContextOptions<mhsDb> options) // default setting untuk database
+        : base(options) { }
+    public DbSet<Mahasiswa> mhs => Set<Mahasiswa>();
+}
